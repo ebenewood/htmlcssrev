51bff72a3dd9d645aa093085e455db4c81171075
eben <Guest@epicodus-42.local>
2016-03-30 20:58:18 -0700

    adds image loading from external site, and reset of that field when reset button is clicked

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,10 @@
+var picList = [
+	'http://www.palazzodiamanti.it/pix/fuoco_86.jpg',
+	'http://www.palazzodiamanti.it/pix/fuoco_225.jpg',
+	'http://www.palazzodiamanti.it/pix/fuoco_36.jpg',
+	'http://www.palazzodiamanti.it/pix/fuoco_167.jpg'
+];
+
 $(function () {
 	$('.btn-blk').click(function (event) {
 		event.preventDefault();
@@ -12,6 +19,7 @@ $(function () {
 
 	 $('.hidlate').slideUp(500);
 	 $('.hid').delay(500).slideUp();
+	 $('#showcase').delay(250).fadeOut(1000)
 	 $('.footer').fadeOut(1000)
 	 $('.jumbotron').delay(1500).fadeIn(1000);
 	 $('.footer').delay(2000).fadeIn(1000)
@@ -22,5 +30,27 @@ $('#mail').click(function (event) {
 
 	alert('email: ebenewood@yahoo.com');
 
+	});
+
+	var clickcount = 0;
+
+	$('#look').click(function (event) {
+		event.preventDefault();
+
+		if (clickcount > 3) {
+			  clickcount = 0
+		};
+
+		$('#showcase').empty();
+		$('#showcase').fadeIn(500);
+		$('.footer').fadeOut();
+
+		var img = $("<img />").attr('src', picList[clickcount])
+	    .on('load', function() {
+
+	      $("#showcase").append(img);
+				clickcount += 1;
+
+	  });
 	});
 });
