3b04947f921d805b884f6f6aafd82a1995889048
eben <Guest@epicodus-42.local>
2016-03-30 19:46:02 -0700

    adds javascript which allows for some movement of jumbotron and divs, with alert of communication href

--- js/scripts.js
+++ js/scripts.js
@@ -6,4 +6,21 @@ $(function () {
 		$('.hid').delay(1000).slideDown(500);
 		$('.hidlate').delay(1500).slideDown(500);
 	});
+
+ $('.reset').click(function (event) {
+	 event.preventDefault();
+
+	 $('.hidlate').slideUp(500);
+	 $('.hid').delay(500).slideUp();
+	 $('.footer').fadeOut(1000)
+	 $('.jumbotron').delay(1500).fadeIn(1000);
+	 $('.footer').delay(2000).fadeIn(1000)
+	});
+
+$('#mail').click(function (event) {
+	event.preventDefault();
+
+	alert('email: ebenewood@yahoo.com');
+
+	});
 });
