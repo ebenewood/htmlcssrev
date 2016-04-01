4dcbc58843ba0e50e162d92d3d8c156e1cf48af8
Eben Wood <ebenewood@yahoo.com>
2016-04-01 06:42:45 -0700

    adds font growing function to js

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,46 @@
 var picList = [
-	'http://www.palazzodiamanti.it/pix/fuoco_86.jpg',
-	'http://www.palazzodiamanti.it/pix/fuoco_225.jpg',
-	'http://www.palazzodiamanti.it/pix/fuoco_36.jpg',
-	'http://www.palazzodiamanti.it/pix/fuoco_167.jpg'
+	'http://www.palazzodiamanti.it/pix/fuoco_86.jpg', //0
+	'http://www.palazzodiamanti.it/pix/fuoco_225.jpg',//1
+	'http://www.palazzodiamanti.it/pix/fuoco_36.jpg', //2
+	'http://www.palazzodiamanti.it/pix/fuoco_167.jpg' //3
 ];
 
+var jumboList = [
+	'?',        //0
+	'bored',    //1
+	'boxed',    //2
+	'filtered', //3
+	'bubbled',  //4
+	'feeling'  //5
+]
+
+function grow (selector) {
+
+	// var i = 0;
+	//
+	// var html =
+	// "<p style='font-size:'" +
+	// ((i + 1) * 10) +
+	// '>' +
+	// jumboList[i] +
+	// '</p>';
+
+	for (i = 0; i <= jumboList.length -1; i ++) {
+
+		selector.prepend(
+		'<p style=\'font-size:' +
+		((i + 1) * 2) +
+		'em\'>' +
+		jumboList[i] +
+		'</p>');
+
+	};
+};
+
 $(function () {
+
+	grow($('.jumbotron'));
+
 	$('.btn-blk').click(function (event) {
 		event.preventDefault();
 
